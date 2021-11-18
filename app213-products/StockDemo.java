3113f29548d2b1743fff8d53ac7be5d6b309ee3e
HakeemKhares <hakeemkhares01@gmail.com>
2021-11-18 18:44:12 +0000

    I'm in pain

--- app213-products/StockDemo.java
+++ app213-products/StockDemo.java
@@ -1,31 +1,38 @@
-
+import java.util.Random;
 /**
  * Demonstrate the StockManager and Product classes.
  * The demonstration becomes properly functional as
  * the StockManager class is completed.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Hakeem Khares
+ * @version 1.0 , 15/11/2021
  */
 public class StockDemo
 {
     // The stock manager.
     private StockList stock;
+    
+    private Random generator = new Random();
 
     /**
      * Create a StockManager and populate it with at least
      * 10 sample products.
      */
-    public StockDemo(StockList stock)
+    public StockDemo()
     {
-        this.stock = stock;
-        
-        // Add at least 10 products, they must be unique to you
-        // Make sure the ids are sequential numbers
+        this.stock = new StockList();
         
         stock.add(new Product(101, "Samsung Galaxy S20"));
         stock.add(new Product(102, "Apple iPhone 12"));
         stock.add(new Product(103, "Google Pixel 4A"));
+        stock.add(new Product(104, "Samsung Galaxy S10"));
+        stock.add(new Product(105, "Apple iPhone 13"));
+        stock.add(new Product(106, "Samsung Galaxy S21"));
+        stock.add(new Product(107, "Samsung Galaxy S21 Ultra"));
+        stock.add(new Product(108, "Samsung Z Flip3"));
+        stock.add(new Product(109, "Samsung Galaxy Buds2"));
+        stock.add(new Product(110, "Apple AirProds Pro"));
+        
     }
     
     /**
@@ -44,15 +51,39 @@ public class StockDemo
         buyProducts();
         stock.print();        
 
-        sellProducts();
-        stock.print();        
+        sellProducts();   
+        stock.print();
     }
     
+    /**
+     * This method will buy different quantities of each of the products
+     * in the stock list.
+     */
     private void buyProducts()
     {
+        stock.buyProduct(101, 500);
+        stock.buyProduct(102, 560);
+        stock.buyProduct(103, 530);
+        stock.buyProduct(104, 520);
+        stock.buyProduct(105, 580);
+        stock.buyProduct(106, 550);
+        stock.buyProduct(107, 550);
+        stock.buyProduct(108, 530);
+        stock.buyProduct(109, 510);
+        stock.buyProduct(110, 610);
     }
 
     private void sellProducts()
     {
+        stock.sellProduct(101, 390);
+        stock.sellProduct(102, 370);
+        stock.sellProduct(103, 360);
+        stock.sellProduct(104, 340);
+        stock.sellProduct(105, 320);
+        stock.sellProduct(106, 350);
+        stock.sellProduct(107, 310);
+        stock.sellProduct(108, 310);
+        stock.sellProduct(109, 360);
+        stock.sellProduct(110, 340);
     }    
 }
